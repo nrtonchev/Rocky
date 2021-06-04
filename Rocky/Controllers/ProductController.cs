5670983b28139505135fa4b6580b67aef908fc3f
Nikolay Tonchev <ntonchev.89@outlook.com>
2021-06-04 21:18:47 +0300

    Adding Product model to DB Adding Product controller and view. Adding First part of Upser Method.

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Products;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Categories.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        //Get for upsert
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+
+            if (id == null)
+            {
+                //this is for create
+                return View(product);
+            }
+
+            else
+            {
+                product = _db.Products.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return View(product);
+            }
+
+        }
+
+        //Post for upsert
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpsertPost(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //Get for Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product obj = _db.Products.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //Post for Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            Product obj = _db.Products.Find(id);
+
+            if (obj == null)
+            {
+                return View(obj);
+            }
+
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
+
--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace Rocky.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<ApplicationType> ApplicationTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
