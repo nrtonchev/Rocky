4f65a6e1b69e5c5ceb3c1d86eeea9a8b2dc96828
Nikolay Tonchev <ntonchev.89@outlook.com>
2021-06-15 21:02:29 +0300

    Adding Cart Controller and View Adding Basic authorization Adding ProductUserViewModel

---
+++ Rocky/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart).ToList();
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(x => x.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Products.Where(x => prodInCart.Contains(x.Id));
+
+            return View(prodList);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(webConstants.SessionCart).ToList();
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(x => x.ProductId == id));
+            HttpContext.Session.Set(webConstants.SessionCart, shoppingCartList);
+        
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
